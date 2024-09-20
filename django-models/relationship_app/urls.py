607921b358d26db0915f8478e48ef14f114200d4
Azeez-Ibrahim <azeez2910@outlook.com>
2024-09-20 22:14:57 +0100

    add new views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import list_books, LibraryDetailView  # Import the views
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views  # Import views from the current app
+from .views.admin_view import admin_view
+from .views.librarian_view import librarian_view
+from .views.member_view import member_view
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Route for the function-based view
@@ -9,4 +12,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # Route for user registration
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Route for user login
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Route for user logout
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
 ]
